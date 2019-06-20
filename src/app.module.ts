65cdd77fc804b230e112a76c4c23c877980b9f7e
hurenfa <420364177@qq.com>
2019-06-20 21:15:39 +0800

    重置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,10 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { PostsController } from './modules/posts.controller';
-import { PostsModule } from './modules/posts.module';
-import { DemoMiddleware } from './core/middlewares/demo.middleware';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { DemoRolesGuard } from './core/guards/demo-roles.guard';
-import { LoggingInterceptor } from './core/interceptors/logging.interceptor';
-// import { DemoService } from './modules/providers/demo/demo.service';
 
 @Module({
-  imports: [PostsModule],
+  imports: [],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: DemoRolesGuard
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: LoggingInterceptor
-    }
-  ],
+  providers: [AppService],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer
-      .apply(DemoMiddleware)
-      .forRoutes('posts');
-  }
-}
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DemoFilter } from './core/fiters/demo.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.useGlobalFilters(new DemoFilter());
   await app.listen(3000);
 }
 bootstrap();
