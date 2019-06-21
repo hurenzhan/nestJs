bc5e3b040e60371a27733cf41bbd2886fef947c2
hurenfa <420364177@qq.com>
2019-06-22 00:56:13 +0800

    提交

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Post } from './post.entity';
+import { Repository } from 'typeorm';
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        @InjectRepository(Post)
+        private readonly postRepository: Repository<Post>
+    ) {}
+
+    async store(data) {
+        const entity = await this.postRepository.create(data)
+        await this.postRepository.save(entity)
+        return entity
+    }
+}
